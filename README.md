429dc0df9cd11055f70169a613fab0b25db484b8
ahmetnurikaynar70 <anurikaynar70@gmail.com>
2023-12-16 01:11:28 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1> JS-AdamAsmaca </h1>
+
+Bu projede basit olarak tasarlanmış bir adam asmaca oyunu hazırlandı. Aradığımız kelimeler dünya şehirleri.
+
+<h2> Kullanılan Teknolojiler <h2>
+
+HTML5 CSS JS
+
+<h2> Sitenin çalışır gif hali </h2>
+
+![](ekran.gif) #JS-AdamAsmaca
---
+++ script.js
@@ -0,0 +1,114 @@
+const word_el = document.getElementById("word");
+const popup = document.getElementById("popup-container");
+const message_el = document.getElementById("success-message");
+const wrongLetters_el = document.getElementById("wrong-letters");
+const items = document.querySelectorAll(".item");
+const message = document.getElementById("message");
+const PlayAgainBtn = document.getElementById("play-again");
+
+const correctLetters = [];
+const wrongLetters = [];
+let selectWord = getRandomWord();
+
+function getRandomWord() {
+  const words = [
+    "istanbul",
+    "moskova",
+    "roma",
+    "budapeste",
+    "prag",
+    "singapur",
+    "milano",
+    "lizbon",
+    "viyana",
+  ];
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+// console.log(getRandomWord());
+
+function displayWord() {
+  word_el.innerHTML = `
+    ${selectWord
+      .split("")
+      .map(
+        (letter) => `
+        <div class="letter">
+            ${correctLetters.includes(letter) ? letter : ""}
+        </div>
+    `
+      )
+      .join("")}
+    `;
+
+  const w = word_el.innerText.replace(/\n/g, "");
+  if (w === selectWord) {
+    popup.style.display = "flex";
+    message_el.innerText = "Tebrikler Kazandınız.";
+  }
+}
+
+function updateWrongLetters() {
+  wrongLetters_el.innerHTML = `
+    ${wrongLetters.length > 0 ? "<h3>Hatalı Harfler</h3>" : ""}
+    ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+    `;
+
+  items.forEach((item, index) => {
+    const errorCount = wrongLetters.length;
+
+    if (index < errorCount) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+
+  if (wrongLetters.length === items.length) {
+    popup.style.display = "flex";
+    message_el.innerText = "Üzgünüm Kaybettiniz.";
+  }
+}
+
+function displayMessage() {
+  message.classList.add("show");
+
+  setTimeout(function () {
+    message.classList.remove("show");
+  }, 2000);
+}
+
+PlayAgainBtn.addEventListener("click", function () {
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = getRandomWord();
+  displayWord();
+  updateWrongLetters();
+
+  popup.style.display = "none";
+});
+
+window.addEventListener("keydown", function (e) {
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if (selectWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+      } else {
+        displayMessage();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+        updateWrongLetters();
+      } else {
+        displayMessage();
+      }
+    }
+  }
+});
+
+displayWord();
